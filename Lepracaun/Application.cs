7d2355233c8cecbe7065dbac2779dc5d34a45e2a
Kouji Matsui <k@kekyo.net>
2022-08-04 19:18:04 +0900

    Added usable invoker.

--- Lepracaun/Application.cs
+++ Lepracaun/Application.cs
@@ -1,4 +1,4 @@
-﻿/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 //
 // Lepracaun - Varies of .NET Synchronization Context.
 // Copyright (c) Kouji Matsui (@kozy_kekyo, @kekyo@mastodon.cloud)
@@ -65,14 +65,105 @@ public class Application : IDisposable
     /// <summary>
     /// Run the application with first time task.
     /// </summary>
-    public void Run(Task task) =>
-        this.context.Run(task);
+    /// <param name="mainTask">Bound main task</param>
+    public void Run(Task mainTask) =>
+        this.context.Run(mainTask);
+
+    /// <summary>
+    /// Run the application with first time task.
+    /// </summary>
+    /// <param name="mainAction">Bound main action</param>
+    public void Run(Func<Task> mainAction) =>
+        this.context.Run(mainAction());
 
     /// <summary>
     /// Request shutdown.
     /// </summary>
     public void Shutdown() =>
         this.context.Shutdown();
+
+    public void Invoke(Action action) =>
+        this.context.Send(_ => action(), null);
+
+    public TResult Invoke<TResult>(Func<TResult> action)
+    {
+        TResult result = default!;
+        this.context.Send(_ => result = action(), null);
+        return result;
+    }
+
+    public void BeginInvoke(Action action) =>
+        this.context.Post(_ => action(), null);
+
+    public Task InvokeAsync(Action action)
+    {
+        var tcs = new TaskCompletionSource<int>();
+        this.context.Post(_ =>
+        {
+            try
+            {
+                action();
+                tcs.TrySetResult(0);
+            }
+            catch (Exception ex)
+            {
+                tcs.TrySetException(ex);
+            }
+        }, null);
+        return tcs.Task;
+    }
+
+    public Task<TResult> InvokeAsync<TResult>(Func<TResult> action)
+    {
+        var tcs = new TaskCompletionSource<TResult>();
+        this.context.Post(_ =>
+        {
+            try
+            {
+                tcs.TrySetResult(action());
+            }
+            catch (Exception ex)
+            {
+                tcs.TrySetException(ex);
+            }
+        }, null);
+        return tcs.Task;
+    }
+
+    public Task InvokeAsync(Func<Task> action)
+    {
+        var tcs = new TaskCompletionSource<int>();
+        this.context.Post(async _ =>
+        {
+            try
+            {
+                await action().ConfigureAwait(false);
+                tcs.TrySetResult(0);
+            }
+            catch (Exception ex)
+            {
+                tcs.TrySetException(ex);
+            }
+        }, null);
+        return tcs.Task;
+    }
+
+    public Task<TResult> InvokeAsync<TResult>(Func<Task<TResult>> action)
+    {
+        var tcs = new TaskCompletionSource<TResult>();
+        this.context.Post(async _ =>
+        {
+            try
+            {
+                tcs.TrySetResult(await action().ConfigureAwait(false));
+            }
+            catch (Exception ex)
+            {
+                tcs.TrySetException(ex);
+            }
+        }, null);
+        return tcs.Task;
+    }
 }
 
 /// <summary>
